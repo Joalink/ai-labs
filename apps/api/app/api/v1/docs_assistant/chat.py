b82d9aaa5519f19f30568144bf0c8192429cb2e6
Joalink <joaquin.hz.p@outlook.com>
2026-05-13 22:52:32 +0000

    feat(docs-assistant): limit ip access for usage

--- apps/api/app/api/v1/docs_assistant/chat.py
+++ apps/api/app/api/v1/docs_assistant/chat.py
@@ -1,11 +1,17 @@
+from app.core.shared.limiter import limiter
 from app.services.docs_assistant.generator import generate_response
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 router = APIRouter()
 
 
 @router.post("/chat")
-async def chat(query: str, namespace: str):
+@limiter.limit("20/minute")
+async def chat(request: Request, query: str):
+    client_ip = request.client.host
+    namespace = f"ip-{client_ip.replace('.', '-').replace(':', '-')}"
+    print(query)
+    print(namespace)
     answer = generate_response(query, namespace)
 
     return {"answer": answer}
